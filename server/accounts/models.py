7d090f975d2e46d1fa6f42e8707587c06fa97922
rubinstory@naver.com <rubinstory@naver.com>
2022-01-07 03:21:45 +0900

    Custom User Model Created

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,46 +1,61 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
-
-        
 class UserManager(BaseUserManager):
-    
-    def create_user(self, email, nickname, password=None):
+    def create_user(self, username, email, password=None):
         if not email:
             raise ValueError('must have user email')
 
-        if not nickname:
-            raise ValueError('must have user nickname')
-
-        # if not name:
-        #     raise ValueError('must have user name')
+        if not username:
+            raise ValueError('must have user username')
 
         user = self.model(
             email = self.normalize_email(email),
-            # name = name,
-            nickname = nickname
+            username = username
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            username=username,
+            email = self.normalize_email(email),
+            password=password,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser):
-    id = models.AutoField(primary_key=True)
-    nickname = models.CharField(default='', max_length=100, null=False, blank =False, unique=True)
+    username = models.CharField(default='', max_length=100, null=False, blank =False, unique=True)
     email = models.EmailField(default='', max_length=100, null =False, blank = False, unique = True)
-    # name = models.CharField(default='', max_length=100, null=False, blank = False)
 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'nickname'
+    USERNAME_FIELD = 'username'
 
-    REUQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email']
 
     def __str__(self):
-        return self.nickname
+        return self.username
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    @is_staff.setter
+    def is_staff(self, value):
+        self._is_staff = value
 
--- server/accounts/serializers.py
+++ server/accounts/serializers.py
@@ -6,12 +6,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             email =validated_data['email'],
-            nickname =validated_data['nickname'],
-            # name =validated_data['name'],
+            username =validated_data['username'],
             password =validated_data['password']
         )
         return user
 
     class Meta:
         model = User
-        fields = ['nickname', 'email', 'password']
+        fields = ['username', 'email', 'password']
