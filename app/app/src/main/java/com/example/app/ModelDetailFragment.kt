0c7735499ca199586f2619c01fec8b7e21efe53a
rubinstory@naver.com <rubinstory@naver.com>
2022-01-07 16:01:59 +0900

    video playing

--- app/app/src/main/java/com/example/app/ModelDetailFragment.kt
+++ app/app/src/main/java/com/example/app/ModelDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.app
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,9 +13,13 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class ModelDetailFragment : Fragment() {
+    private lateinit var videoAdapter: VideoAdapter
+
     lateinit var myInfluencer: InfluencerItem
     private var _binding: ModelDetailFragmentBinding? = null
     private val binding get() = _binding!!
+
+    var videoList = mutableListOf<VideoItem>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,31 +28,48 @@ class ModelDetailFragment : Fragment() {
         _binding = ModelDetailFragmentBinding.inflate(inflater, container, false)
         loadInfluencer()
         setFollowButton()
+        setVideoAdapter()
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        videoAdapter = VideoAdapter(this.requireContext())
+        videoAdapter.itemList = videoList
+        binding.modelDetailViewpager.adapter = videoAdapter
     }
-    
+
     override fun onDestroyView() {
         super.onDestroyView()
+        videoList.clear()
         _binding = null
     }
 
+    fun setVideoAdapter() {
+        for(i in 0..10) {
+//            videoList.add(VideoItem("http://www.antenna.co.kr/upload/keyVisual/2015121711353535351ba.mp4"))
+//            videoList.add(VideoItem("https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4"))
+        }
+        videoList.add(VideoItem("https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4"))
+        videoList.add(VideoItem("https://assets.mixkit.co/videos/preview/mixkit-winter-fashion-cold-looking-woman-concept-video-39874-large.mp4"))
+        videoList.add(VideoItem("https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-fashion-woman-with-silver-makeup-39875-large.mp4"))
+    }
+
+
     fun setFollowButton() {
-        binding.twoBtnBar.modelContractBtn.setOnClickListener({
-            activity!!.supportFragmentManager.beginTransaction()
+        binding.twoBtnBar.modelContractBtn.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment, LoginFragment())
                 .commit()
-        })
+        }
     }
 
     fun setDataToView() {
         binding.modelProfileView.modelDetailName.text = myInfluencer.full_name
         binding.modelProfileView.modelDetailCountry.text = myInfluencer.country
     }
-    
+
     fun loadInfluencer() {
         var call: Call<InfluencerItem> = DjangoAPICall.API.get_influencer_by_pk(1)
         call.enqueue(object: Callback<InfluencerItem> {
---
+++ app/app/src/main/java/com/example/app/VideoItem.kt
@@ -0,0 +1,9 @@
+package com.example.app
+
+class VideoItem {
+    lateinit var url: String
+
+    constructor(videoUrl: String) {
+        this.url = videoUrl
+    }
+}
