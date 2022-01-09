01c75cd82dd783dfac07c8e11ccd8676a2745c6f
root <asdf0322@kaist.ac.kr>
2022-01-09 07:05:37 +0000

    smart block chain

--- server/contract/urls.py
+++ server/contract/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from rest_framework import routers
 from contract.views import ContractViewSet
 from rest_framework import viewsets
+from contract import views
 
 router = routers.DefaultRouter()
 router.register('Contracts', ContractViewSet)
@@ -11,4 +12,7 @@ router.register('Contracts', ContractViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('mine_block/', views.mine_block),
+    path('get_chain/', views.get_chain),
+    path('new_transaction/', views.new_transaction),
 ]
